310cce0b739ebb04f4d3a1f0f4d74db0ed0557f6
Ioana <ioana.sauleac@yahoo.com>
2022-06-18 16:48:43 +0200

    starting ramblings

--- hanoi-visual-solver/src/App.tsx
+++ hanoi-visual-solver/src/App.tsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./styles/styles.module.css";
+
+type Disc = {
+  position: number;
+  value: number;
+};
 
 function App() {
-  return <div className="App"> Hanoi towers</div>;
+  const UserInput = () => {
+    const [discs, setDiscs] = useState(0);
+
+    const handleSubmit = (event: any) => {
+      solveInConsole(discs);
+      event.preventDefault();
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Discs:
+          <input
+            type="text"
+            //value={discs}
+            onChange={(event) => setDiscs(Number(event.target.value))}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  };
+
+  const HanoiArea = () => {
+    return <div className={styles.hanoiContainer}> </div>;
+  };
+
+  //TODO: clean up the mumbo jumbo below
+
+  const CreateDiscs = (discs: number) => {
+    console.log(discs);
+    //discs as arrays
+    let discsArray: Disc[] = [];
+
+    if (discs > 1) {
+      for (let idx = 0; idx < discs; idx++) {
+        discsArray.push({
+          position: idx,
+          value: idx,
+        });
+      }
+    } else console.log("eeek");
+    return discsArray.reverse();
+  };
+
+  const solveInConsole = (discs: number) => {
+    let initialDiscs = CreateDiscs(discs);
+    let middleDiscs: Disc[] = [];
+    let solvedDiscs: Disc[] = [];
+
+    console.log("initialDiscs", initialDiscs);
+    //console.log(initialDiscs.length);
+    console.log("middleDiscs", middleDiscs);
+    console.log("solvedDiscs", solvedDiscs);
+
+    //assume discs > 0, then check if needs validation
+
+    //ending solution solvedDiscs.lenght == discs; ????
+  };
+
+  //aux
+  const getTop = (sourceArray: Disc[]) => sourceArray[sourceArray.length - 1];
+
+  //TODO: use a proper data structure ;)
+
+  return (
+    <div className="App">
+      <h1> Hanoi towers </h1>
+      <UserInput />
+      <HanoiArea />
+    </div>
+  );
 }
 
 export default App;
---
+++ hanoi-visual-solver/src/components/userInput.tsx
@@ -0,0 +1,28 @@
+//TODO: move component
+
+// import { useState } from "react";
+
+// export const UserInput = () => {
+//   const [discs, setDiscs] = useState(0);
+
+//   const handleSubmit = (event: any) => {
+//     console.log(discs);
+//     event.preventDefault();
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label>
+//         Name:
+//         <input
+//           type="text"
+//           value={discs}
+//           onChange={(event) => setDiscs(Number(event.target.value))}
+//         />
+//       </label>
+//       <input type="submit" value="Submit" />
+//     </form>
+//   );
+// };
+
+export {};
