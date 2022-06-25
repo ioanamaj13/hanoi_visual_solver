ef1921a7665593366addeb567a9c881a0da65632
ioanamaj13 <iasauleac@gmail.com>
2022-06-25 12:39:04 +0100

    simple solver in console

--- hanoi-visual-solver/src/App.tsx
+++ hanoi-visual-solver/src/App.tsx
@@ -1,18 +1,46 @@
 import React, { useState } from "react";
 import styles from "./styles/styles.module.css";
 
-type Disc = {
-  position: number;
-  value: number;
+type Peg = {
+  name: string;
+  contents: string;
 };
+
+type RecordedMove = {
+  disc: number;
+  source: Peg;
+  target: Peg;
+  message: string;
+};
+
+let moveLog: RecordedMove[] = [];
 
 function App() {
   const UserInput = () => {
     const [discs, setDiscs] = useState(0);
+    let source = CreateDiscs(discs);
+    let aux: Peg = { name: "Aux", contents: "" };
+    let destination: Peg = { name: "Destination", contents: "" };
 
     const handleSubmit = (event: any) => {
-      solveInConsole(discs);
+      hanoiSolveInConsole(source, destination, aux, discs);
       event.preventDefault();
+    };
+
+    const hanoiSolveInConsole = function (
+      source: Peg,
+      destination: Peg,
+      aux: Peg,
+      discs: number
+    ) {
+      if (discs <= 0) {
+        return;
+      }
+
+      hanoiSolveInConsole(source, aux, destination, discs - 1);
+      let message = `Move Disk ${discs} from ${source.name.toUpperCase()} to ${destination.name.toUpperCase()}`;
+      moveDisc(discs, source, destination, message);
+      hanoiSolveInConsole(aux, destination, source, discs - 1);
     };
 
     return (
@@ -21,7 +49,6 @@ function App() {
           Discs:
           <input
             type="text"
-            //value={discs}
             onChange={(event) => setDiscs(Number(event.target.value))}
           />
         </label>
@@ -36,41 +63,44 @@ function App() {
 
   //TODO: clean up the mumbo jumbo below
 
-  const CreateDiscs = (discs: number) => {
-    console.log(discs);
-    //discs as arrays
-    let discsArray: Disc[] = [];
+  const CreateDiscs = (discs: number): Peg => {
+    let discsArray: string = "";
 
     if (discs > 1) {
-      for (let idx = 0; idx < discs; idx++) {
-        discsArray.push({
-          position: idx,
-          value: idx,
-        });
+      for (let idx = discs; idx > 0; idx--) {
+        discsArray += idx.toString();
       }
-    } else console.log("eeek");
-    return discsArray.reverse();
-  };
+    } else console.log("not enough discs");
 
-  const solveInConsole = (discs: number) => {
-    let initialDiscs = CreateDiscs(discs);
-    let middleDiscs: Disc[] = [];
-    let solvedDiscs: Disc[] = [];
-
-    console.log("initialDiscs", initialDiscs);
-    //console.log(initialDiscs.length);
-    console.log("middleDiscs", middleDiscs);
-    console.log("solvedDiscs", solvedDiscs);
-
-    //assume discs > 0, then check if needs validation
-
-    //ending solution solvedDiscs.lenght == discs; ????
+    return { name: "Source", contents: discsArray };
   };
 
   //aux
-  const getTop = (sourceArray: Disc[]) => sourceArray[sourceArray.length - 1];
+  const moveDisc = (
+    disc: number,
+    source: Peg,
+    target: Peg,
+    message: string
+  ) => {
+    // console.log("source", source);
+    source.contents = source.contents.slice(0, source.contents.length - 1);
 
-  //TODO: use a proper data structure ;)
+    target.contents += disc.toString();
+
+    moveLog.push({
+      disc: disc,
+      source: { name: source.name, contents: source.contents },
+      target: {
+        name: target.name,
+        contents: target.contents,
+      },
+      message: message,
+    });
+
+    console.log(moveLog);
+  };
+
+  //TODO: check data structure
 
   return (
     <div className="App">
